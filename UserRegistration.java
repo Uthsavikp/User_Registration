0ade1563e26a0edd0e86c1fedb4608549ef26357
uthsa <uthsavi29@gmail.com>
2020-12-17 20:46:46 +0530

    Added:To Enter Valid First Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,19 @@
 package registration;
 
+import java.util.Scanner;
+
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration Problem");
+        boolean flag;
+        Scanner user = new Scanner(System.in);
+        do {
+            String firstName = ("[A-Z][a-zA-Z]{2,}");
+            System.out.println("Enter Your First Name: ");
+            String userInput = user.next();
+            flag = userInput.matches(firstName);
+            if (!flag) System.out.println("Re Enter Your Name With Capital And With Minimum 3 Characters: ");
+        } while (!flag);
+        System.out.println("Valid First Name");
     }
 }
