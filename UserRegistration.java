4ea7ef506966addd0288db2a20096eef0e7117b0
uthsa <uthsavi29@gmail.com>
2020-12-17 22:07:59 +0530

    Added:To Enter Valid Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,15 +5,27 @@ import java.util.Scanner;
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration Problem");
+
         boolean flag;
         Scanner user = new Scanner(System.in);
         do {
-            String firstName = ("[A-Z][a-zA-Z]{2,}");
+            String firstName = "[A-Z][a-zA-Z]{2,}";
             System.out.println("Enter Your First Name: ");
             String userInput = user.next();
             flag = userInput.matches(firstName);
             if (!flag) System.out.println("Re Enter Your Name With Capital And With Minimum 3 Characters: ");
         } while (!flag);
         System.out.println("Valid First Name");
+
+        do {
+            String lastName = ("[A-Z][a-zA-Z]{2,}");
+            System.out.println("Enter Your Last Name: ");
+            String userInput = user.next();
+            flag = userInput.matches(lastName);
+            if (!flag) System.out.println("Re Enter Your Name With Capital And With Minimum 3 Characters: ");
+        } while (!flag);
+        System.out.println("Valid Last Name");
     }
 }
+
+
